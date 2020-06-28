4503feed370e28e71b95917f521a35ff6098f84a
Paige <paigemacgregor33@gmail.com>
2020-06-27 18:43:30 -0700

    fix errors and styling

--- src/App.js
+++ src/App.js
@@ -3,40 +3,38 @@ import './App.css';
 
 class App extends Component {
   state = {
-    permission: false
+    permissions: false
   }
 
   permission_handler = () => {
+    let currentComponent = this;
     var permission;
     if (!("Notification" in window)) {
       alert("This browser does not support desktop notification");
     }
     else if (Notification.permission === "granted") {
       permission = true;
-      this.setState({permission: permission}, this.notification_handler);
+      currentComponent.setState({permissions: permission}, currentComponent.notification_handler);
     }
     else if (Notification.permission !== "denied") {
       Notification.requestPermission().then(function (permission) {
         if (permission === "granted") {
           permission = true;
-          this.setState({permission: permission}, this.notification_handler);
+          currentComponent.setState({permissions: permission}, currentComponent.notification_handler);
         }
       });
     }
   }
 
   notification_handler = () => {
-    if(this.state.permission){
+    if(this.state.permissions){
       setInterval(function(){
-        var notification = new Notification("Hi there!");
+        var options = {
+          body: 'Look 20 feet away for 20 seconds!'
+        }
+        var notification = new Notification("Give your eyes a break", options);
       }, 20 * 60 * 1000);
     } 
-  }
-
-  outputHTML = (permission) => {
-    if(!permission) {
-      return <button className="permissions_msg btn" onClick={this.permission_handler}>Turn on notifications!</button>
-    }
   }
 
   componentDidMount() {
@@ -48,7 +46,6 @@ class App extends Component {
       <div className="App">
         <main>
           <h1>Work Health Balance</h1>
-          {this.outputHTML(this.state.permission)}
           <p>This app helps you follow the <span className="first_section">&#9679; 20 20 20 &#9679;</span> rule for eye health</p>
           <p>Every <span>20</span> minutes, you'll receive a notification from us, when you do, look <span>20</span> feet away for <span>20</span> seconds!</p>
         </main>
